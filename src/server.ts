ac9e944c3b3c4aaeecea57b2a778ad1e3232b97e
AzFlin <azflin1@gmail.com>
2025-02-22 13:25:37 -0500

    add 3000 port for swagger

--- src/server.ts
+++ src/server.ts
@@ -31,7 +31,7 @@ export async function buildServer(): Promise<FastifyInstance> {
           description: "API documentation for the Oracle Framework",
           version: "1.0.0",
         },
-        host: "localhost",
+        host: "localhost:3000",
         schemes: ["http"],
         consumes: ["application/json"],
         produces: ["application/json"],
